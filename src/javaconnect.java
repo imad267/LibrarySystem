3357c2b3e12f7807434342f6557dc270296ded76
imad zetouneh <imad.ez955@gmail.com>
2020-06-12 21:26:31 +0200

    upload starting files

---
+++ src/javaconnect.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kaddo
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect 
+{
+ Connection conn;
+ 
+ public static Connection connectDB ()
+         {
+           try 
+           {
+             Class.forName("org.sqlite.JDBC");
+             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\kaddo\\OneDrive\\Documents\\NetBeansProjects\\Library Managment System\\LibraryDataBase.sqlite");
+             return conn;
+           }
+           catch (Exception e)
+            {
+                JOptionPane.showMessageDialog(null,e);
+                return null;
+                
+                
+            }
+             
+             
+             
+         }
+         
+         
+         
+         
+}
